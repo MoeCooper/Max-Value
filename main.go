29560d9eff4e895c2cad329b5faf74317b76bbba
Maurice <jc70240@gmail.com>
2020-10-30 08:10:49 +0000

    Initial commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+    "fmt"
+    "math"
+)
+
+func main() {
+	numbers := []int{16,8,42,4,56,9}
+
+    max := math.MinInt16
+
+    for i := range numbers {
+        if numbers[i] > max {
+            max = numbers[i]
+        }
+    }
+    fmt.Println(max)
+    
+}
